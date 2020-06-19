8b06ab2a8919deab98ef90f3827a7fc1b0231e5b
Aurélien Roux <aureroux13@gmail.com>
2020-06-19 09:20:06 -0400

    bypass request with cached assets

--- sw.js
+++ sw.js
@@ -11,6 +11,7 @@ const assets = [
   "/css/styles.css",
   "/img/dish.png",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v52/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
 ];
 
 // install service worker
@@ -31,4 +32,9 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
   // console.log('fetch event', evt)
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request)
+    })
+  )
 })
